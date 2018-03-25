c1e7b9404a9f683c3aab5e4006376fd8ab9fb831
Marc Donges <marc.donges@gmail.com>
2018-03-25 19:20:08 +0200

    Move PulseProcessor and useful files around to myon subdirectory. Update DAQconfig to reflect that New anaDAQ for different calling convention of class.

---
+++ myon/anaDAQ.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+# code fragment  anaDAQ.py to run inside runDAQ.py
+'''
+  code fragment to embed user code (in pulsfilter.py) into
+   script runDAQ.py
+'''
+
+# ->>> code from here inserted as 'anaDAQ.py' in runDAQ.py
+
+from picodaqa.mpBDisplay import mpBDisplay
+from picodaqa.mpHists import mpHists
+
+# import analysis code as library
+from myon.PulseProcessor import PulseProcessor
+from myon.mpPulseDisplay import *
+
+# pulse shape analysis
+filtRateQ = None
+histQ = None
+filtRateQ = mp.Queue(1) # information queue for Filter
+procs.append(mp.Process(name='RMeter',
+          target = mpRMeter, 
+          args=(filtRateQ, 12., 2500., 'muon rate history') ) )
+#               mp.Queue  rate  update interval          
+
+histQ = mp.Queue(1) # information queue for Filter
+#  book histograms and start histogrammer
+Hdescriptors = []
+Hdescriptors.append([0., 0.4, 50, 20., 'noise Trg. Pulse (V)', 0] )
+#                   min max nbins ymax    title               lin/log
+Hdescriptors.append([0., 0.8, 50, 15., 'valid Trg. Pulse (V)', 0] )
+Hdescriptors.append([0., 0.8, 50, 15., 'Pulse height (V)', 0] )
+Hdescriptors.append([0., 15., 45, 7.5, 'Tau (µs)', 1] )
+procs.append(mp.Process(name='Hists',
+          target = mpHists, 
+          args=(histQ, Hdescriptors, 2000., 'Filter Histograms') ) )
+#             data Queue, Hist.Desrc  interval    
+
+VSigQ = mp.Queue(1) # information queue for Filter
+mode = 2 # 0:signed, 1: abs. 2: symmetric
+size = 1. # stretch factor for display
+procs.append(mp.Process(name = 'ChannelSignals',
+          target = mpBDisplay, 
+          args=(VSigQ, PSconf, mode, size, 'Panel Signals') ) )
+#               mp.Queue Chan.Conf.           name          
+
+PulseQ = mp.Queue(1)
+procs.append(mp.Process(name = 'PulseDisplay', target = mpPulseDisplay, args=(PulseQ,PSconf)))
+
+
+
+# run pulse analysis
+cId_pf = BM.BMregister() # get a Buffer Manager Client Id
+
+  # pulse analysis as thread
+#thrds.append(threading.Thread(target=pulseFilter,
+#      args = ( BM, PSconf, cId, filtRateQ, histQ, VSigQ, True, 1) ) )
+#                      BMclientId  RMeterQ  histQ  fileout verbose    
+
+pulseProcessor = PulseProcessor.PulseProcessor(BM, PSconf, cId_pf, filtRateQ, histQ, VSigQ, PulseQ, True, 2)
+procs.append(mp.Process(name = "pulseProcessor", target = pulseProcessor.process))
+
+  # pulse analysis as sub-process
+# procs.append(
+#     mp.Process(
+#         name = 'pulseFilterd', target = pulseFilterd,
+#         args = (BM, PSconf, cId_pf),
+#         kwargs = {
+#             'filterRateQueue':    filtRateQ,
+#             'histogramQueue':     histQ,
+#             'VoltageSignalQueue': VSigQ,
+#             'PulseDisplayQueue':  PulseQ,
+#             'logPulses':          True,
+#             'verbosity':          2
+#         }
+#     )
+# )
+#                      BMclientId  RMeterQ  histQ  fileout verbose    
+
+#   could also run this in main thread
+#pulseFilter( BM, PSconf, cId, filtRateQ, histQ, VSigQ, True, 1)  
+
+# <<< - end of inserted code
